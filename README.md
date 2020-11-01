8ee2752df607a09ad93929f819df40d46fa62f4b
FR98 <ros18676@uvg.edu.gt>
2020-10-31 23:20:02 -0600

    Improve package settings

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # jet-python
 JSON Encrypted Token - Python module
+
+```python
+from jet import JET
+from jet.utils import hmac_sha256
+
+
+GLOBAL_JET = JET(
+    SECRET = 'my-secret-string'
+)
+
+user_secret = hmac_sha256('user-password', 'user-password', 'ascii')
+
+payload = {
+    'id': 1,
+    'message': "Hola"
+}
+
+# Generate token
+token = GLOBAL_JET.encrypt(user_secret, payload)
+
+# Get info on token
+decrypted_meta, decrypted_payload = GLOBAL_JET.decrypt(user_secret, token)
+
+```
--- setup.py
+++ setup.py
@@ -5,18 +5,26 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="json-encrypted-token",
-    version="0.0.1",
+    version="0.0.2",
     author="Francisco Rosal",
     author_email="frangrosalo@hotmail.com",
     description="JET - JSON Encrypted Token",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/FR98/jet-python",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('venv', 'tests')),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'backports.pbkdf2==0.1',
+        'cffi==1.14.3',
+        'cryptography==3.1.1',
+        'pkg-resources==0.0.0',
+        'pycparser==2.20',
+        'six==1.15.0',
     ],
     python_requires='>=3.6',
 )
